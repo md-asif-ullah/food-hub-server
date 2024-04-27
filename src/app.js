57580b1f1dd19a6209cbb71caaaeacd488ec3ffd
MD.Asif Ullah <mdasifullah456@gmail.com>
2024-04-27 22:01:43 +0600

    added middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import createError from "http-errors";
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    limit: 5,
+    message: "Too many requests , please try again after 5 minutes.",
+});
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(limiter);
+
+//routes
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.use((err, req, res, next) => {
+    return next(createError({ statusCode: err.status, message: err.message }));
 });
 
 export default app;
