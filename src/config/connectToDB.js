1fb702a33485d6d553625479400c5f86c1674f41
MD.Asif Ullah <mdasifullah456@gmail.com>
2024-04-27 20:27:48 +0600

    connected to DB

---
+++ src/config/connectToDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+    try {
+        const connected = await mongoose.connect(process.env.MONGODB_URL);
+        if (connected) {
+            console.log("Connected to DB successfully!");
+        }
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        process.exit(1);
+    }
+};
+
+export default connectToDB;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import express from "express";
+import app from "./app.js";
+import connectToDB from "./config/connectToDB.js";
 
-const app = express();
+const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(port, async () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    await connectToDB();
 });
