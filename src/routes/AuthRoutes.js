2513ce8dae0d4ef256d842d0e4d73c6c831c65dc
MD.Asif Ullah <mdasifullah456@gmail.com>
2024-05-04 10:26:28 +0600

    logOut functionality added

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { userLoginValidation } from "../validations/userInputValidation.js";
 import inputErrorhandling from "../middleware/zodInputValidate.js";
 import { isLoggedOut } from "../middleware/auth.js";
-import { loginUser } from "../controllers/AuthController.js";
+import { loginUser, logoutUser } from "../controllers/AuthController.js";
 
 const authRoutes = express.Router();
 
@@ -12,5 +12,7 @@ authRoutes.post(
     inputErrorhandling(userLoginValidation),
     loginUser
 );
+
+authRoutes.get("/logout", logoutUser);
 
 export default authRoutes;
