316d400074cd34dd7baa97d0ea219292e2f6f8e6
MD.Asif Ullah <mdasifullah456@gmail.com>
2024-07-05 11:10:17 +0600

    update origin

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://food-hub-client-kappa.vercel.app",
+        origin: "http://localhost:5000",
     })
 );
 app.use(express.urlencoded({ extended: true }));
